6acec8d0217eeb7741580d2db10bc983c019fb8c
Himanshi Solanki <himanshisolanki115@gmail.com>
2025-09-20 16:51:16 +0530

    intitial Commit

--- demo_python_01.py
+++ demo_python_01.py
@@ -9,3 +9,10 @@ age = 25
 
 # Print variables
 print("My name is", name)
+print("I am", age, "years old")
+# Simple function
+def add_numbers(a, b):
+    return a + b
+
+# Call the function
+result = add_numbers(10, 20)
